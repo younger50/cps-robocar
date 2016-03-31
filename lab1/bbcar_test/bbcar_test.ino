012476a02d1b314e49e1a03c835fd0b34746cc2f
younger50 <pt54088@gmail.com>
2016-03-31 15:47:50 +0800

    [modify] can read senscor threshold from button

--- lab1/bbcar_test/bbcar_test.ino
+++ lab1/bbcar_test/bbcar_test.ino
@@ -27,20 +27,22 @@
 
 */
 
-int sPinR = A0;
-int sPinL = A1;
+int sPinR = A0;   // sensor R input
+int sPinL = A1;   // sensor L input
+int tPin = A5;   // sensor threshold pin
 int mPinR = 10;
 int mPinL = 11;
 int sValueR = 0;   // sensor value R
 int sValueL = 0;   // sensor value L
+int tValue = 0;
 int msR = 0;    // timer of right servo
 int statR = 0;   // stat (0 in LOW, 1 inHIGH)
 int tRH = 1300;   // timer thresthhold of right servo PWM HIGH signal
-int tRL = 700;   // timer thresthhold of right servo PWM LOW signal
+int tRL = 20000;   // timer thresthhold of right servo PWM LOW signal
 int msL = 0;
 int statL = 0;
 int tLH = 1700;
-int tLL = 300;
+int tLL = 20000;
 
 void setup() {
   // declare the ledPin as an OUTPUT:
@@ -55,6 +57,8 @@ void loop() {
   // read the value from the sensor:
   sValueR = analogRead(sPinR);
   sValueL = analogRead(sPinL);
+  tValue = analogRead(tPin);
+  //led_diplay(sValueR>>1, sValueR>>2, sValueR>>3, sValueR>>4);
   //Serial.println(sValueR);
   int time = micros();
   if ( time - msR > tRH && statR == 1) {
@@ -62,7 +66,7 @@ void loop() {
     msR = time;
     statR = 0;
   }
-  if ( time - msR > tRL && statR == 0 && sValueR > 500) {
+  if ( time - msR > tRL && statR == 0 && sValueR > tValue) {
     digitalWrite(mPinR, HIGH);
     msR = time;
     statR = 1;
@@ -72,10 +76,26 @@ void loop() {
     msL = time;
     statL = 0;
   }
-  if ( time - msL > tLL && statL == 0 && sValueL > 500) {
+  if ( time - msL > tLL && statL == 0 && sValueL > tValue) {
     digitalWrite(mPinL, HIGH);
     msL = time;
     statL = 1;
   }
   delayMicroseconds(10);
 }
+
+int led_diplay(int v1, int v2, int v3, int v4) {
+    int pin1 = 2;
+    int pin2 = 3;
+    int pin3 = 4;
+    int pin4 = 5;
+    pinMode(pin1, OUTPUT);
+    pinMode(pin2, OUTPUT);
+    pinMode(pin3, OUTPUT);
+    pinMode(pin4, OUTPUT);
+    digitalWrite(pin1, v1);
+    digitalWrite(pin2, v2);
+    digitalWrite(pin3, v3);
+    digitalWrite(pin4, v4);
+}
+
